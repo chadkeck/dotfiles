33300d8ef3ee1249d6667d25f879985ea8a52fe2
Chad Bibler <chadkeck@gmail.com>
2012-09-10 15:21:38 -0500

    More improvements.

--- install.sh
+++ install.sh
@@ -5,3 +5,8 @@ for i in `ls -A | egrep '^\.'`
 do
     cp $i ~
 done
+
+# install oh-my-zsh
+curl -L https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh | sh
+
+cp chadkeck.zsh-theme ~/.oh-my-zsh/themes/
